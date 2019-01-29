d4c7a0fabf9219a3c50a343910e5fd8e3a569e82
Ali Onur Geven <ali.geven@sap.com>
2019-01-29 23:12:32 +0300

    Add vendoring structure and route tests

--- app.go
+++ app.go
@@ -7,87 +7,88 @@ import (
 
 	"gopkg.in/mgo.v2/bson"
 
-	"github.com/gorilla/mux"
 	. "github.com/csvital/find_my_mall/config"
 	. "github.com/csvital/find_my_mall/dao"
 	. "github.com/csvital/find_my_mall/models"
+	"github.com/gorilla/mux"
 )
 
 var config = Config{}
 var dao = ShoppingMallsDAO{}
 
-// GET list of shopping_malls
+// AllShoppingMalls GET list of shopping_malls
 func AllShoppingMalls(w http.ResponseWriter, r *http.Request) {
-	shopping_malls, err := dao.FindAll()
+	shoppingMall, err := dao.FindAll()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, shopping_malls)
+	respondWithJSON(w, http.StatusOK, shoppingMall)
 }
 
-// GET a shopping manll by its ID
+// FindShoppingMall GET a shopping manll by its ID
 func FindShoppingMall(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	shopping_mall, err := dao.FindById(params["id"])
+	shoppingMall, err := dao.FindById(params["id"])
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid Shopping Mall ID")
 		return
 	}
-	respondWithJson(w, http.StatusOK, shopping_mall)
+	respondWithJSON(w, http.StatusOK, shoppingMall)
 }
 
-// POST a new shopping mall
+// CreateShoppingMall POST a new shopping mall
 func CreateShoppingMall(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	var shopping_mall ShoppingMall
-	if err := json.NewDecoder(r.Body).Decode(&shopping_mall); err != nil {
+	var shoppingMall ShoppingMall
+	if err := json.NewDecoder(r.Body).Decode(&shoppingMall); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
-	shopping_mall.ID = bson.NewObjectId()
-	if err := dao.Insert(shopping_mall); err != nil {
+	shoppingMall.ID = bson.NewObjectId()
+	if err := dao.Insert(shoppingMall); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusCreated, shopping_mall)
+	respondWithJSON(w, http.StatusCreated, shoppingMall)
 }
 
-// PUT update an existing shopping mall
+// UpdateShoppingMall PUT update an existing shopping mall
 func UpdateShoppingMall(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	var shopping_mall ShoppingMall
-	if err := json.NewDecoder(r.Body).Decode(&shopping_mall); err != nil {
+	var shoppingMall ShoppingMall
+	if err := json.NewDecoder(r.Body).Decode(&shoppingMall); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
-	if err := dao.Update(shopping_mall); err != nil {
+	if err := dao.Update(shoppingMall); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
-// DELETE an existing shopping mall
+// DeleteShoppingMall DELETE an existing shopping mall
 func DeleteShoppingMall(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	var shopping_mall ShoppingMall
-	if err := json.NewDecoder(r.Body).Decode(&shopping_mall); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	if err := dao.Delete(shopping_mall); err != nil {
+	params := mux.Vars(r)
+	shoppingMall, err := dao.FindById(params["id"])
+	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+	if err := dao.Delete(shoppingMall); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJson(w, code, map[string]string{"error": msg})
+	respondWithJSON(w, code, map[string]string{"error": msg})
 }
 
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
@@ -108,10 +109,13 @@ func main() {
 	r.HandleFunc("/shoppingMalls", AllShoppingMalls).Methods("GET")
 	r.HandleFunc("/shoppingMalls", CreateShoppingMall).Methods("POST")
 	r.HandleFunc("/shoppingMalls", UpdateShoppingMall).Methods("PUT")
-	r.HandleFunc("/shoppingMalls", DeleteShoppingMall).Methods("DELETE")
+	r.HandleFunc("/shoppingMalls/{id}", DeleteShoppingMall).Methods("DELETE")
 	r.HandleFunc("/shoppingMalls/{id}", FindShoppingMall).Methods("GET")
 
-	if err := http.ListenAndServe(":3000", r); err != nil {
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
 		log.Fatal(err)
+	} else {
+		log.Println("Successfully connected")
 	}
 }
---
+++ app_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"testing"
+
+	. "github.com/csvital/find_my_mall/models"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var dummyID string
+
+func CreateDummyShoppingMall() {
+	dummyMall := &ShoppingMall{ID: bson.NewObjectId(), Name: "dummyMall", District: "dummyDistrict", City: "dummyCity"}
+	jsonStr, err := json.Marshal(dummyMall)
+	if err != nil {
+		log.Fatal(err)
+	}
+	url := "http://localhost:3000/shoppingMalls"
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	res, _ := http.DefaultClient.Do(req)
+	if res.StatusCode != 201 {
+		log.Fatal("Cannot create dummy connection. Status code: ", res.Status)
+	}
+}
+
+func GetAllDummyShoppingMalls() {
+	url := "http://localhost:3000/shoppingMalls"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	if res.StatusCode != 200 {
+		log.Fatal("Cannot delete dummy connection. Status codesss: ", res.Status)
+	}
+	defer res.Body.Close()
+	decoder := json.NewDecoder(res.Body)
+	var data []map[string]interface{}
+	err := decoder.Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dummyID = data[0]["id"].(string)
+}
+
+func DeleteDummyShoppingMall() {
+	url := "http://localhost:3000/shoppingMalls/" + dummyID
+	req, _ := http.NewRequest("DELETE", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	if res.StatusCode != 200 {
+		log.Fatal("Cannot delete dummy connection. Status code: ", res.Status)
+	}
+}
+
+func PutDummyShoppingMall() {
+	dummyMall := &ShoppingMall{ID: bson.ObjectIdHex(dummyID), Name: "dummyMallChanged", District: "dummyDistrictChanged", City: "dummyCityChanged"}
+	jsonStr, err := json.Marshal(dummyMall)
+	if err != nil {
+		log.Fatal(err)
+	}
+	url := "http://localhost:3000/shoppingMalls"
+	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer(jsonStr))
+	res, _ := http.DefaultClient.Do(req)
+	if res.StatusCode != 200 {
+		log.Fatal("Cannot create dummy connection. Status code: ", res.Status)
+	}
+}
+
+func FindDummyShoppingMall() {
+	url := "http://localhost:3000/shoppingMalls/" + dummyID
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	if res.StatusCode != 200 {
+		log.Fatal("Cannot delete dummy connection. Status code: ", res.Status)
+	}
+}
+func TestAppRoutes(t *testing.T) {
+	CreateDummyShoppingMall()
+	GetAllDummyShoppingMalls()
+	PutDummyShoppingMall()
+	FindDummyShoppingMall()
+	DeleteDummyShoppingMall()
+}
